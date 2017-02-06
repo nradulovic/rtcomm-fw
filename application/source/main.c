75aba126af2a94704a641f61b44262c2f3db1c1d
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2017-02-06 14:59:26 +0100

    Removed initialization of unused stuff

--- application/source/main.c
+++ application/source/main.c
@@ -1,19 +1,14 @@
 
 /*=========================================================  INCLUDE FILES  ==*/
 
-#include <epa_i2c_master.h>
 #include "main.h"
 #include "epa_main.h"
 #include "epa_acq.h"
 #include "epa_aux.h"
 #include "epa_heartbeat.h"
-#include "epa_calibration.h"
-#include "epa_adt7410.h"
-#include "epa_eeprom.h"
 #include "bsp.h"
 #include "app_stat.h"
 #include "ctrl_bus.h"
-#include "nv_data.h"
 #include "notify.h"
 #include "acq_sync.h"
 
@@ -38,15 +33,12 @@ int main(void)
 {
     bsp_init_hal();
 
-    /* Initialize NonVolatile data module
-     */
-    nv_data_init();
 
     /* NOTE:
 	 * Initialize SYNC output pin, this pin will be used to synchronize
 	 * the ADC.
 	 */
-	 acq_sync_init();
+	acq_sync_init();
 
 	/* Initialize the portable core. Portable core handles interrupts, timer
      * and CPU initialization. After initialization the core timer is started
@@ -71,8 +63,6 @@ int main(void)
      */
     nevent_register_mem(&g_generic_heap.mem_class);
 
-    nepa_init(&g_epa_i2c_master,       &g_epa_i2c_master_define);
-
     /* Initialize Acquisition EPA. This EPA responsibility is handling of X, Y
      * and Z ADC and sending data to Linux
      */
@@ -86,22 +76,6 @@ int main(void)
     /* Status LED EPA. Signals if everything is OK.
      */
     nepa_init(&g_epa_heartbeat,	&g_epa_heartbeat_define);
-
-    /* Initialize EEPROM driver EPA
-     */
-    nepa_init(&g_epa_eeprom,    &g_epa_eeprom_define);
-
-    /* Initialize FRAM driver EPA
-	 */
-    nepa_init(&g_epa_fram,		&g_epa_fram_define);
-
-    /* Initialize ADT7410 driver EPA
-     */
-    nepa_init(&g_epa_adt7410, 	&g_epa_adt7410_define);
-
-    /* Initialize Calibration EPA
-     */
-    nepa_init(&g_epa_calibration, &g_epa_calibration_define);
 
     /* Initialize the main EPA. This EPA controls will dispatch control events
      * to all other EPAs.
