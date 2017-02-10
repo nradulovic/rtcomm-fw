766e403c3a759f3efdcc4b811c9afd7aeabe8ecb
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2017-02-10 10:10:01 +0100

    Work on RTCOMM communication

---
+++ application/include/rtcomm.h
@@ -0,0 +1,18 @@
+/*
+ * rtcomm.h
+ *
+ *  Created on: Feb 9, 2017
+ *      Author: nenad
+ */
+
+#ifndef APPLICATION_INCLUDE_RTCOMM_H_
+#define APPLICATION_INCLUDE_RTCOMM_H_
+
+#include "cdi/cdi_rtcomm.h"
+
+struct rtcomm
+{
+
+};
+
+#endif /* APPLICATION_INCLUDE_RTCOMM_H_ */
---
+++ application/source/rtcomm.c
@@ -0,0 +1,14 @@
+/*
+ * rtcomm.c
+ *
+ *  Created on: Feb 9, 2017
+ *      Author: nenad
+ */
+
+
+#include "rtcomm.h"
+
+void rtcomm_init(struct rtcomm * rtcomm, void * buffer)
+{
+
+}
