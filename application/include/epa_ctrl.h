e7ede1a417384644ee9c84316fcf6064c697e6e6
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2017-05-31 14:50:54 +0200

    Moved code from ctrl.c to epe_ctrl.c

--- application/include/epa_ctrl.h
+++ application/include/epa_ctrl.h
@@ -10,7 +10,9 @@
 
 /*=========================================================  INCLUDE FILES  ==*/
 
-#include "epacon.h"
+#include "config/epacon.h"
+#include "cdi/io.h"
+#include "neon_eds.h"
 
 /*===============================================================  MACRO's  ==*/
 /*------------------------------------------------------  C++ extern begin  --*/
@@ -18,12 +20,49 @@
 extern "C" {
 #endif
 /*============================================================  DATA TYPES  ==*/
+
+enum ctrl_epa_events
+{
+	CTRL_EVT_CONFIG = EPA_CTRL_EVENT_BASE,
+	CTRL_EVT_PARAM
+};
+
+struct ctrl_evt_config
+{
+	struct nevent				super;
+	struct io_ctrl_config		config;
+};
+
+struct ctrl_evt_param
+{
+	struct nevent				super;
+	struct io_ctrl_param		param;
+};
+
+struct ctrl
+{
+	I2C_HandleTypeDef			i2c;
+	void *						buffer;
+	uint16_t					size;
+};
+
 /*======================================================  GLOBAL VARIABLES  ==*/
 
-extern struct nepa_define g_ctrl_epa_define;
-extern struct nepa g_ctrl_epa;
+extern struct nepa_define 		g_ctrl_epa_define;
+extern struct nepa 				g_ctrl_epa;
+
+extern struct ctrl 				g_ctrl;
 
 /*===================================================  FUNCTION PROTOTYPES  ==*/
+
+/* -- Methods to be used by ISRs -------------------------------------------- */
+
+void ctrl_isr_rx_complete(struct ctrl * ctrl);
+
+void ctrl_isr_tx_complete(struct ctrl * ctrl);
+
+void ctrl_isr_error(struct ctrl * ctrl);
+
 /*--------------------------------------------------------  C++ extern end  --*/
 #ifdef __cplusplus
 }
--- application/source/epa_ctrl.c
+++ application/source/epa_ctrl.c
@@ -7,11 +7,12 @@
 
 /*=========================================================  INCLUDE FILES  ==*/
 
+#include <string.h>
 #include "epa_ctrl.h"
+#include "epa_controller.h"
 #include "neon_eds.h"
 #include "cdi/io.h"
 #include "status.h"
-#include "ctrl.h"
 
 /*=========================================================  LOCAL MACRO's  ==*/
 /*======================================================  LOCAL DATA TYPES  ==*/
@@ -19,10 +20,10 @@
 struct ctrl_wspace
 {
 	struct netimer				timeout;
-	union ctrl_var_buffer {
+	union ctrl_data_buff {
 		struct io_ctrl_config   	config;
 		struct io_ctrl_param		param;
-	}							buffer;
+	}							buff;
 };
 
 enum ctrl_local_events
@@ -50,8 +51,84 @@ struct nepa_define              g_ctrl_epa_define = NEPA_DEF_INIT(
         sizeof(g_ctrl_event_queue), "ctrl", EPA_CTRL_PRIO);
 
 struct nepa                     g_ctrl_epa;
+struct ctrl 					g_ctrl;
 
 /*============================================  LOCAL FUNCTION DEFINITIONS  ==*/
+
+/* -- Helper function ------------------------------------------------------- */
+
+static void ctrl_rx_complete(struct ctrl * ctrl, void * buffer, uint16_t size)
+{
+	static const struct nevent sig_xfer_complete = NEVENT_INITIALIZER(
+			SIG_XFER_COMPLETE, NULL, 0);
+	nerror						error;
+
+	(void)ctrl;
+	(void)buffer;
+	(void)size;
+
+	error = nepa_send_event_i(&g_ctrl_epa, &sig_xfer_complete);
+
+	if (error) {
+		status_warn(STATUS_RESOURCE_FAILED);
+	}
+}
+
+static void ctrl_tx_complete(struct ctrl * ctrl, void * buffer, uint16_t size)
+{
+	(void)ctrl;
+	(void)buffer;
+	(void)size;
+}
+
+static void ctrl_error(struct ctrl * ctrl, uint32_t error)
+{
+	static const struct nevent sig_err_busy = NEVENT_INITIALIZER(SIG_ERR_BUSY,
+			NULL, 0);
+	nerror					neon_error;
+	const struct nevent *	event;
+
+	(void)ctrl;
+
+	if (error == 0) {
+		event = &sig_err_busy;
+	} else {
+		event = &sig_err_busy;
+	}
+	/*
+	 * Usually this functions is called from ISR. In that case we call _i
+	 * methods. The only exception is when this functions is called with
+	 * error argument equal to zero. This means that it is called from loop
+	 * code and we must call non _i methods.
+	 */
+	if (ncore_is_lock_valid()) {
+		neon_error = nepa_send_event_i(&g_ctrl_epa, event);
+	} else {
+		neon_error = nepa_send_event(&g_ctrl_epa, event);
+	}
+
+	if (neon_error) {
+		status_warn(STATUS_RESOURCE_FAILED);
+	}
+	status_warn(STATUS_CTRL_FAILED);
+}
+
+static void ctrl_start_rx(struct ctrl * ctrl, void * buffer, uint16_t size)
+{
+	HAL_StatusTypeDef			status = HAL_BUSY;
+
+	if (HAL_I2C_GetState(&ctrl->i2c) == HAL_I2C_STATE_READY) {
+		ctrl->buffer = buffer;
+		ctrl->size = size;
+		status = HAL_I2C_Slave_Receive_IT(&ctrl->i2c, (uint8_t *)buffer, size);
+	}
+
+	if (status != HAL_OK) {
+		ctrl_error(ctrl, 0u);
+	}
+}
+
+/* - State machine states --------------------------------------------------- */
 
 static naction state_init(struct nsm * sm, const nevent * event)
 {
@@ -59,8 +136,24 @@ static naction state_init(struct nsm * sm, const nevent * event)
 
     switch (nevent_id(event)) {
         case NSM_INIT: {
+        	return (naction_handled());
+        }
+        case CONTROLLER_SIG_INIT: {
         	netimer_init(&ws->timeout);
+        	nepa_send_signal(&g_controller_epa, CONTROLLER_SIG_INIT_DONE);
 
+        	{
+        		nepa_send_signal(&g_ctrl_epa, SIG_XFER_COMPLETE);
+
+        		ctrl_header_pack(&ws->buff.config.header, sizeof(ws->buff.config));
+        		ws->buff.config.en_x = 1;
+        		ws->buff.config.en_y = 1;
+        		ws->buff.config.en_z = 1;
+        		ws->buff.config.en_probe_buffer = 0;
+        		ws->buff.config.probe_mux_hi = 3;
+        		ws->buff.config.probe_mux_lo = 2;
+        		ws->buff.config.probe_pga = 1;
+        	}
         	return (naction_transit_to(sm, state_wait_config));
         }
         default: {
@@ -75,7 +168,7 @@ static naction state_wait_config(struct nsm * sm, const nevent * event)
 
 	switch (nevent_id(event)) {
 		case NSM_ENTRY: {
-			ctrl_start_rx(&g_ctrl, &ws->buffer.config, sizeof(ws->buffer.config));
+			ctrl_start_rx(&g_ctrl, &ws->buff.config, sizeof(ws->buff.config));
 
 			return (naction_handled());
 		}
@@ -85,8 +178,33 @@ static naction state_wait_config(struct nsm * sm, const nevent * event)
 			return (naction_handled());
 		}
 		case SIG_XFER_COMPLETE: {
+			struct ctrl_evt_config * config;
+			uint32_t			dummy;
+			bool				is_valid;
+			nerror				neon_error = !NERROR_NONE;
 
-			return (naction_transit_to(sm, state_wait_param));
+			is_valid = ctrl_header_unpack(&ws->buff.config.header, &dummy);
+
+			if (is_valid == false) {
+				status_warn(STATUS_CTRL_FAILED);
+
+				return (naction_transit_to(sm, state_wait_config));
+			}
+			config = NEVENT_CREATE(struct ctrl_evt_config, CTRL_EVT_CONFIG);
+
+			if (config) {
+				memcpy(&config->config, &ws->buff.config,
+						sizeof(config->config));
+				neon_error = nepa_send_event(&g_controller_epa, &config->super);
+			}
+
+			if (neon_error) {
+				status_warn(STATUS_RESOURCE_FAILED);
+
+				return (naction_transit_to(sm, state_wait_config));
+			} else {
+				return (naction_transit_to(sm, state_wait_param));
+			}
 		}
 		case SIG_ERR_BUSY: {
 			netimer_after(&ws->timeout, NTIMER_MS(10), SIG_RETRY);
@@ -108,7 +226,7 @@ static naction state_wait_param(struct nsm * sm, const nevent * event)
 
 	switch (nevent_id(event)) {
 		case NSM_ENTRY: {
-			ctrl_start_rx(&g_ctrl, &ws->buffer.param, sizeof(ws->buffer.param));
+			ctrl_start_rx(&g_ctrl, &ws->buff.param, sizeof(ws->buff.param));
 
 			return (naction_handled());
 		}
@@ -118,6 +236,28 @@ static naction state_wait_param(struct nsm * sm, const nevent * event)
 			return (naction_handled());
 		}
 		case SIG_XFER_COMPLETE: {
+			struct ctrl_evt_param * param;
+			bool					is_valid;
+			uint32_t				dummy;
+			nerror					neon_error = !NERROR_NONE;
+
+			is_valid = ctrl_header_unpack(&ws->buff.config.header, &dummy);
+
+			if (is_valid == false) {
+				status_warn(STATUS_CTRL_FAILED);
+
+				return (naction_transit_to(sm, state_wait_param));
+			}
+			param = NEVENT_CREATE(struct ctrl_evt_param, CTRL_EVT_PARAM);
+
+			if (param) {
+				memcpy(&param->param, &ws->buff.param, sizeof(param->param));
+				neon_error = nepa_send_event(&g_controller_epa, &param->super);
+			}
+
+			if (neon_error) {
+				status_warn(STATUS_RESOURCE_FAILED);
+			}
 
 			return (naction_transit_to(sm, state_wait_param));
 		}
@@ -127,7 +267,7 @@ static naction state_wait_param(struct nsm * sm, const nevent * event)
 			return (naction_handled());
 		}
 		case SIG_RETRY: {
-			return (naction_transit_to(sm, state_wait_config));
+			return (naction_transit_to(sm, state_wait_param));
 		}
 		default: {
 			return (naction_handled());
@@ -137,50 +277,20 @@ static naction state_wait_param(struct nsm * sm, const nevent * event)
 
 /*===========================================  GLOBAL FUNCTION DEFINITIONS  ==*/
 
-void ctrl_rx_complete(struct ctrl * ctrl, void * buffer, uint16_t size)
+void ctrl_isr_rx_complete(struct ctrl * ctrl)
 {
-	static const struct nevent sig_xfer_complete = NEVENT_INITIALIZER(
-			SIG_XFER_COMPLETE, NULL, 0);
-	nerror						error;
-
-	(void)ctrl;
-	(void)buffer;
-	(void)size;
-
-	error = nepa_send_event_i(&g_ctrl_epa, &sig_xfer_complete);
-
-	if (error) {
-		status_warn(STATUS_RESOURCE_FAILED);
-	}
+	ctrl_rx_complete(ctrl, ctrl->buffer, ctrl->size);
 }
 
-void ctrl_tx_complete(struct ctrl * ctrl, void * buffer, uint16_t size)
+void ctrl_isr_tx_complete(struct ctrl * ctrl)
 {
-	(void)ctrl;
-	(void)buffer;
-	(void)size;
+	ctrl_tx_complete(ctrl, ctrl->buffer, ctrl->size);
 }
 
-void ctrl_error(struct ctrl * ctrl, uint32_t error)
+void ctrl_isr_error(struct ctrl * ctrl)
 {
-	static const struct nevent sig_err_busy = NEVENT_INITIALIZER(SIG_ERR_BUSY,
-			NULL, 0);
-
-	(void)ctrl;
-	(void)error;
-
-	if (error == 0) {
-		nerror					neon_error;
-
-		neon_error = nepa_send_event_i(&g_ctrl_epa, &sig_err_busy);
-
-		if (neon_error) {
-			status_warn(STATUS_RESOURCE_FAILED);
-		}
-	}
-	status_warn(STATUS_CTRL_FAILED);
+	ctrl_error(ctrl, HAL_I2C_GetError(&ctrl->i2c));
 }
-
 /*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
 /** @endcond *//***************************************************************
  * END of epa_ctrl.c
